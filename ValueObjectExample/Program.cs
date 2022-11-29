3adde0d89d49a234337ec595acb7560a8c894033
Edson <Edson@DESKTOP-D0BM5A4>
2022-11-29 19:39:36 -0300

    Valeu object class Address and tests

--- ValueObjectExample/Program.cs
+++ ValueObjectExample/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ValueObjectExample;
+
 Console.WriteLine("Hello, World!");
+
+Address addressOne = new Address("Rua Henrique Dias", "Pesqueira", "Pernambuco", "Brazil", "55200000");
+Address addressTwo = new Address("Rua Henrique Dias", "Pesqueira", "Pernambuco", "Brazil", "55200000");
+
+Console.WriteLine(EqualityComparer<Address>.Default.Equals(addressOne, addressTwo));
+Console.WriteLine(object.Equals(addressOne, addressTwo));
+Console.WriteLine(addressOne.Equals(addressTwo));
+Console.WriteLine(addressOne == addressTwo); // False
